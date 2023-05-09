3b13a006910e641b50a27ac62bf6cc22697e0c0d
Ivan Kovachev <ivann9963@gmail.com>
2023-05-09 13:23:24 +0300

    GRAD-8: Add user roles and update unit test

---
+++ GradeCenter.Data/Migrations/20230509100458_AddUserRoles.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GradeCenter.Data.Migrations
+{
+    public partial class AddUserRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserRole",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserRole",
+                table: "AspNetUsers");
+        }
+    }
+}
--- GradeCenter.Data/Models/Account/User.cs
+++ GradeCenter.Data/Models/Account/User.cs
@@ -5,6 +5,16 @@ namespace GradeCenter.Data.Models.Account
     public class User : IdentityUser
     {
         public bool IsActive { get; set; } = false;
+        public UserRoles UserRole { get; set; }
+    }
+
+    public enum UserRoles
+    {
+        Admin = 0,
+        Principle = 1,
+        Teacher = 2,
+        Parent = 3,
+        Student = 4
     }
 }
 
