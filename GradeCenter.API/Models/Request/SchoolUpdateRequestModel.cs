b5bf8a5cf41a9eae082eed70b9d76ebc04ed4c0a
aleksanderstoyanov <blackeder3@gmail.com>
2023-05-11 16:17:27 +0300

    GRAD-16: Add School service and controller

---
+++ GradeCenter.API/Models/Request/SchoolUpdateRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GradeCenter.API.Models.Request
+{
+    public class SchoolUpdateRequestModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ GradeCenter.Services/Schools/ISchoolService.cs
@@ -0,0 +1,18 @@
+﻿using GradeCenter.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCenter.Services.Schools
+{
+    public interface ISchoolService
+    {
+        School? GetSchoolById(int id);
+        Task Create(string newName, string newAddress, int principalId, string principalFirstName, string principalLastName);
+        Task Update(int id, string newName, string newAddress);
+        Task Delete(int id);
+        Task<Principal> EnsurePrincipalCreated(string newFirstName, string newLastName);
+    }
+}
