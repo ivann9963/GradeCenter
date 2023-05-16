7a10f13b9f5fdceb409a83dac786e7893b0ba8a6
ivyo <ivyo@WIN-0OVOR0CIS69>
2023-05-16 23:10:57 +0300

    GRAD-47: Add CurricullumController and Service

---
+++ GradeCenter.API/Controllers/CurriculumController.cs
@@ -0,0 +1,58 @@
+﻿using GradeCenter.Data.Models;
+using GradeCenter.Data.Models.Account;
+using GradeCenter.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GradeCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurriculumController : ControllerBase
+    {
+        private readonly UserManager<AspNetUser> _userManager;
+        private readonly ICurriculumService _curriculumService;
+
+        public CurriculumController(ICurriculumService curriculumService, UserManager<AspNetUser> userManager)
+        {
+            this._curriculumService = curriculumService;
+            _userManager = userManager;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(List<Discipline> curriculum, SchoolClass schoolClass)
+        {
+            var checkedReqeust = await ValidateRequest();
+
+            if (checkedReqeust != null)
+                return checkedReqeust;
+
+            _curriculumService.Create(curriculum, schoolClass);
+
+            return Ok();
+        }
+
+        private async Task<IActionResult> ValidateRequest()
+        {
+            var loggedUser = await GetLoggedUser();
+
+            if (loggedUser == null || !IsAdmin(loggedUser))
+                return Unauthorized();
+
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid model state.");
+
+            return null;
+        }
+
+        private async Task<AspNetUser> GetLoggedUser()
+        {
+            return await _userManager.FindByNameAsync(User.Identity.Name);
+        }
+
+        private bool IsAdmin(AspNetUser user)
+        {
+            return user.UserRole.Equals(UserRoles.Admin);
+        }
+    }
+}
---
+++ GradeCenter.Services/CurricullumService.cs
@@ -0,0 +1,53 @@
+﻿using GradeCenter.Data;
+using GradeCenter.Data.Models;
+
+namespace GradeCenter.Services
+{
+    public class CurricullumService : ICurriculumService
+    {
+        private readonly GradeCenterContext _db;
+
+        public CurricullumService(GradeCenterContext db)
+        {
+            _db = db;
+        }
+
+        public void Create(List<Discipline> disciplines, SchoolClass schoolClass)
+        {
+            schoolClass = CreateSchedule(disciplines, schoolClass);
+
+            _db.SchoolClasses.Add(schoolClass);
+            _db.SaveChanges();
+        }
+        public void Delete(Discipline discipline)
+        {
+            discipline.IsActive = false;
+
+            _db.Disciplines.Update(discipline);
+            _db.SaveChanges();
+        }
+
+        public void Update(List<Discipline> disciplines, SchoolClass schoolClass)
+        {
+            schoolClass.Curriculum = disciplines;
+
+            _db.SchoolClasses.Update(schoolClass);
+            _db.SaveChanges();
+        }
+
+        private SchoolClass CreateSchedule(List<Discipline> disciplines, SchoolClass schoolClass)
+        {
+            var allClassesInYear = _db.SchoolClasses.Where(sc => sc.Year == schoolClass.Year);
+
+            foreach (var yearClass in allClassesInYear)
+            {
+                if (yearClass.Curriculum.Count == 0)
+                    continue;
+
+                //TODO:...
+            }
+
+            return null;
+        }
+    }
+}
