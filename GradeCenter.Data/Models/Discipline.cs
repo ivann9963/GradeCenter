ce27ea451da3499b1eeb30bfd152c03879b3a12d
ivyo <ivyo@WIN-0OVOR0CIS69>
2023-05-16 23:10:22 +0300

    GRAD-47: Add Migration for SchoolClass and Discipline

---
+++ GradeCenter.Data/Models/Discipline.cs
@@ -0,0 +1,20 @@
+﻿using GradeCenter.Data.Models.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace GradeCenter.Data.Models
+{
+    public class Discipline
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Occurance { get; set; }
+
+        public SchoolClass SchoolClass { get; set; }
+
+        public AspNetUser Teacher { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ GradeCenter.Data/Models/SchoolClass.cs
@@ -0,0 +1,19 @@
+﻿using GradeCenter.Data.Models.Account;
+
+namespace GradeCenter.Data.Models
+{
+    public class SchoolClass
+    {
+        public Guid Id { get; set; }
+
+        public int Year { get; set; }
+
+        public string Department { get; set; }
+
+        public AspNetUser HeadTeacher { get; set; }
+
+        public List<AspNetUser> Students { get; set; }
+
+        public List<Discipline> Curriculum { get; set; }
+    }
+}
