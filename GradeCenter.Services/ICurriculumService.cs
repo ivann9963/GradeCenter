9143f43e34438ed775a8e7db39338912518e09a1
ivyo <ivyo@WIN-0OVOR0CIS69>
2023-05-16 23:10:57 +0300

    GRAD-47: Add CurricullumController and Service

---
+++ GradeCenter.Services/ICurriculumService.cs
@@ -0,0 +1,11 @@
+﻿using GradeCenter.Data.Models;
+
+namespace GradeCenter.Services
+{
+    public interface ICurriculumService
+    {
+        void Create(List<Discipline> disciplines, SchoolClass schoolClass);
+        void Update(List<Discipline> disciplines, SchoolClass schoolClass);
+        void Delete(Discipline discipline);
+    }
+}
