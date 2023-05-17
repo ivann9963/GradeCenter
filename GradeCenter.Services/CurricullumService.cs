710f9a63eb79636af3bb11571c398302382d13ff
ivyo <ivyo@WIN-0OVOR0CIS69>
2023-05-18 00:31:16 +0300

    GRAD-47: Refactor CurricullumService

--- GradeCenter.Services/CurricullumService.cs
+++ GradeCenter.Services/CurricullumService.cs
@@ -1,15 +1,11 @@
 ﻿using GradeCenter.Data;
 using GradeCenter.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace GradeCenter.Services
 {
     public class CurriculumService : ICurriculumService
     {
         private readonly GradeCenterContext _db;
-        private static readonly Random Random = new Random();
 
         public CurriculumService(GradeCenterContext db)
         {
@@ -20,23 +16,33 @@ namespace GradeCenter.Services
         {
             var curriculum = GenerateCurriculum(disciplines);
 
-            AddDisciplines(curriculum);
+            _db.Disciplines.AddRange(curriculum);
         }
 
         public void Update(List<Discipline> disciplines)
         {
             var updatedCurriculum = GenerateCurriculum(disciplines);
 
-            UpdateDisciplines(updatedCurriculum);
+            _db.Disciplines.UpdateRange(updatedCurriculum);
         }
 
         public void Delete(List<Discipline> disciplines)
         {
             disciplines.ForEach(d => d.IsActive = false);
             
-            UpdateDisciplines(disciplines);
+            _db.Disciplines.UpdateRange(disciplines);
         }
 
+
+        /// <summary>
+        /// The GenerateCurriculum method creates a school curriculum. 
+        /// It assigns each subject in the list a day and time, ensuring
+        /// no clashes with the teacher's other classes or the class's other subjects. 
+        /// It returns the list of subjects with their assigned slots, if available. 
+        /// If the subject list is invalid, it returns an empty curriculum.
+        /// </summary>
+        /// <param name="disciplines"></param>
+        /// <returns></returns>
         private List<Discipline> GenerateCurriculum(List<Discipline> disciplines)
         {
             List<Discipline> currentCurriculum = new();
@@ -62,6 +68,42 @@ namespace GradeCenter.Services
         private KeyValuePair<DayOfWeek, TimeSpan>? GenerateAvailableDayAndTime(List<KeyValuePair<DayOfWeek, TimeSpan>> teachersClasses, SchoolClass schoolClass, List<Discipline> currentCurriculum)
         {
             // Generate all possible time slots
+            var allTimeSlots = GenerateAllTimeSlots();
+
+            // Shuffle the list
+            var randomTimeSlots = allTimeSlots.OrderBy(x => Guid.NewGuid()).ToList();
+            
+            // Find a free time slot
+            var freeSlot = FindFreeTimeSlot(teachersClasses, currentCurriculum, randomTimeSlots);
+
+            return freeSlot;
+        }
+
+        private static KeyValuePair<DayOfWeek, TimeSpan>? FindFreeTimeSlot(List<KeyValuePair<DayOfWeek, TimeSpan>> teachersClasses, List<Discipline> currentCurriculum, List<KeyValuePair<DayOfWeek, TimeSpan>> randomTimeSlots)
+        {
+            KeyValuePair<DayOfWeek, TimeSpan>? freeSlot = null;
+
+            foreach (var slot in randomTimeSlots)
+            {
+                var day = slot.Key;
+                var time = slot.Value;
+
+                var schoolClassIsBusy = currentCurriculum.Any(d => d.OccuranceDay == day && d.OccuranceTime == time && d.IsActive);
+                var teacherIsBusy = teachersClasses.Any(c => c.Key == day && c.Value == time);
+                var maximumDisciplinesReached = currentCurriculum.Count(d => d.OccuranceDay == day) >= 7;
+
+                if (!schoolClassIsBusy && !teacherIsBusy && !maximumDisciplinesReached)
+                {
+                    freeSlot = slot;
+                    break;
+                }
+            }
+
+            return freeSlot;
+        }
+
+        private List<KeyValuePair<DayOfWeek, TimeSpan>> GenerateAllTimeSlots()
+        {
             var allTimeSlots = new List<KeyValuePair<DayOfWeek, TimeSpan>>();
             foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
             {
@@ -76,32 +118,10 @@ namespace GradeCenter.Services
                 }
             }
 
-            // Shuffle the list
-            var randomTimeSlots = allTimeSlots.OrderBy(x => Random.Next()).ToList();
-            KeyValuePair<DayOfWeek, TimeSpan>? freeSlot = null;
-
-            // Find a free time slot
-            foreach (var slot in randomTimeSlots)
-            {
-                var day = slot.Key;
-                var time = slot.Value;
-
-                var schoolClassIsBusy = currentCurriculum.Any(d => d.OccuranceDay == day && d.OccuranceTime == time);
-                var teacherIsBusy = teachersClasses.Any(c => c.Key == day && c.Value == time);
-                var disciplinesForDay = currentCurriculum.Count(d => d.OccuranceDay == day);
-                var maximumDisciplinesReached = disciplinesForDay >= 7;
-
-                if (!schoolClassIsBusy && !teacherIsBusy && !maximumDisciplinesReached)
-                {
-                    freeSlot = slot;
-                    break;
-                }
-            }
-
-            return freeSlot;
+            return allTimeSlots;
         }
 
-        private bool IsValid(List<Discipline> disciplines)
+        private static bool IsValid(List<Discipline> disciplines)
         {
             var year = disciplines?.FirstOrDefault()?.SchoolClass.Year;
             return year != null && !disciplines.Any(d => d.SchoolClass.Year != year);
@@ -110,35 +130,11 @@ namespace GradeCenter.Services
         private List<KeyValuePair<DayOfWeek, TimeSpan>> GetTeacherClasses(Discipline discipline)
         {
             return _db.Disciplines
-                .Where(t => t.Teacher == discipline.Teacher && t.SchoolClass.Year == discipline.SchoolClass.Year)
+                .Where(t => t.Teacher == discipline.Teacher && t.SchoolClass.Year == discipline.SchoolClass.Year && discipline.IsActive)
                 .OrderBy(d => d.OccuranceDay)
                 .ThenBy(t => t.OccuranceTime)
                 .Select(x => new KeyValuePair<DayOfWeek, TimeSpan>(x.OccuranceDay, x.OccuranceTime))
                 .ToList();
-        }
-
-        private void AddDisciplines(List<Discipline> disciplines)
-        {
-            ExecuteDbTransaction(() => _db.Disciplines.AddRange(disciplines));
-        }
-
-        private void UpdateDisciplines(List<Discipline> disciplines)
-        {
-            ExecuteDbTransaction(() => _db.Disciplines.UpdateRange(disciplines));
-        }
-
-        private void ExecuteDbTransaction(Action dbAction)
-        {
-            try
-            {
-                dbAction();
-                _db.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-                throw;
-            }
         }
     }
 }
