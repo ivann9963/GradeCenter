724ac141d6ef293d83fa41e18ec0d4629b12d8be
aleksanderstoyanov <blackeder3@gmail.com>
2023-05-29 23:07:41 +0300

    GRAD-57: Add Grade Model

---
+++ GradeCenter.Data/Models/Grade.cs
@@ -0,0 +1,19 @@
+﻿using GradeCenter.Data.Models.Account;
+using System.ComponentModel.DataAnnotations;
+
+namespace GradeCenter.Data.Models
+{
+    public class Grade
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Range(2, 6)]
+        public int Number { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public Discipline Discipline { get; set; }
+
+        public AspNetUser Student { get; set; }
+    }
+}
